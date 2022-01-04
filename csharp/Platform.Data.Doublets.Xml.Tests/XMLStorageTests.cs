71e15288b03c24f2905aeade17ccc03705a01a5d
FreePhoenix888 <freephoenix888@gmail.com>
2022-01-04 12:01:49 +0600

    Rename `CreateLinks` parameter

--- csharp/Platform.Data.Doublets.Xml.Tests/XMLStorageTests.cs
+++ csharp/Platform.Data.Doublets.Xml.Tests/XMLStorageTests.cs
@@ -21,10 +21,10 @@ namespace Platform.Data.Doublets.Xml.Tests
             defaultXmlStorage.CreateDocument("documentFilename"); 
         }
         
-        private static ILinks<TLink> CreateLinks<TLink>(string dataDBFilename)
+        private static ILinks<TLink> CreateLinks<TLink>(string linksDbFilename)
         {
             var linksConstants = new LinksConstants<TLink>(enableExternalReferencesSupport: true);
-            return new UnitedMemoryLinks<TLink>(new FileMappedResizableDirectMemory(dataDBFilename),
+            return new UnitedMemoryLinks<TLink>(new FileMappedResizableDirectMemory(linksDbFilename),
                 UnitedMemoryLinks<TLink>.DefaultLinksSizeStep, linksConstants, IndexTreeType.Default);
         }
 
