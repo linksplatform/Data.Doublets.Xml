b7042add3cea914349c30fe205b507ea6818a1a8
FreePhoenix888 <freephoenix888@gmail.com>
2022-01-04 12:02:53 +0600

    Make Tests public

--- csharp/Platform.Data.Doublets.Xml.Tests/XMLStorageTests.cs
+++ csharp/Platform.Data.Doublets.Xml.Tests/XMLStorageTests.cs
@@ -8,26 +8,26 @@ namespace Platform.Data.Doublets.Xml.Tests
     public class XMLStorageTests
     {
         [Fact]
-        private void CounstructorTest()
+        public void CounstructorTest()
         {
             var links = CreateLinks();
             new DefaultXmlStorage<ulong>(links);
         }
         
         [Fact]
-        private void CreateDocumentTest(){
+        public void CreateDocumentTest(){
             var links = CreateLinks();
             var defaultXmlStorage = new DefaultXmlStorage<ulong>(links);
             defaultXmlStorage.CreateDocument("documentFilename"); 
         }
         
-        private static ILinks<TLink> CreateLinks<TLink>(string linksDbFilename)
+        public static ILinks<TLink> CreateLinks<TLink>(string linksDbFilename)
         {
             var linksConstants = new LinksConstants<TLink>(enableExternalReferencesSupport: true);
             return new UnitedMemoryLinks<TLink>(new FileMappedResizableDirectMemory(linksDbFilename),
                 UnitedMemoryLinks<TLink>.DefaultLinksSizeStep, linksConstants, IndexTreeType.Default);
         }
 
-        private static ILinks<ulong> CreateLinks() => CreateLinks<ulong>(new Platform.IO.TemporaryFile());
+        public static ILinks<ulong> CreateLinks() => CreateLinks<ulong>(new Platform.IO.TemporaryFile());
     }
 }
