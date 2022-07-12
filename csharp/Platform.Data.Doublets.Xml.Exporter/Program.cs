9c492ed0a93f1717cca0ec990c08ec64620f6ca7
FreePhoenix888 <freephoenix888@gmail.com>
2022-07-12 15:06:14 +0600

    Add importer and exporter console apps

---
+++ csharp/Platform.Data.Doublets.Xml.Exporter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Xml;
+using Platform.Data.Doublets.Memory;
+using Platform.Data.Doublets.Memory.United.Generic;
+using Platform.Data.Doublets.Sequences.Converters;
+using Platform.IO;
+using Platform.Memory;
+using TLinkAddress = System.UInt64;
+
+namespace Platform.Data.Doublets.Xml.Exporter
+{
+    public class XmlExporter
+    {
+        public static void Main(params string[] args)
+        {
+            var argumentIndex = 0;
+            var linksFilePath = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "Links file path", args);
+            var xmlFilePath = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "XML file path", args);
+            var defaultDocumentName = Path.GetFileNameWithoutExtension(xmlFilePath);
+            var documentName = ConsoleHelpers.GetOrReadArgument(argumentIndex, $"Document name (default: {defaultDocumentName})", args);
+            if (string.IsNullOrWhiteSpace(documentName))
+            {
+                documentName = defaultDocumentName;
+            }
+            if (!File.Exists(linksFilePath))
+            {
+                Console.WriteLine($"${linksFilePath} file does not exist.");
+            }
+            using FileStream xmlFileStream = new(xmlFilePath, FileMode.Append);
+            var xmlWriterSettings = new XmlWriterSettings()
+            {
+                Indent = true
+            };
+            var xmlWriter = XmlWriter.Create(xmlFileStream, xmlWriterSettings);
+            var linksConstants = new LinksConstants<TLinkAddress>(enableExternalReferencesSupport: true);
+            using UnitedMemoryLinks<TLinkAddress> memoryAdapter = new (new FileMappedResizableDirectMemory(linksFilePath), UnitedMemoryLinks<TLinkAddress>.DefaultLinksSizeStep, linksConstants, IndexTreeType.Default);
+            var links = memoryAdapter.DecorateWithAutomaticUniquenessAndUsagesResolution();
+            var balancedVariantConverter = new BalancedVariantConverter<TLinkAddress>(links);
+            var storage = new DefaultXmlStorage<TLinkAddress>(links, balancedVariantConverter);
+            var exporter = new Platform.Data.Doublets.Xml.XmlExporter<TLinkAddress>(storage);
+            var document = storage.GetDocument(documentName);
+            using ConsoleCancellation cancellation = new ();
+            var cancellationToken = cancellation.Token;
+            Console.WriteLine("Press CTRL+C to stop.");
+            exporter.Export(xmlWriter, document, cancellationToken);
+        }
+    }
+}
---
+++ csharp/Platform.Data.Doublets.Xml.Importer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Xml;
+using Platform.Data.Doublets.Memory;
+using Platform.Data.Doublets.Memory.United.Generic;
+using Platform.Data.Doublets.Sequences.Converters;
+using Platform.IO;
+using Platform.Memory;
+using TLinkAddress = System.UInt64;
+
+namespace Platform.Data.Doublets.Xml.Importer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var argumentIndex = 0;
+            var xmlFilePath = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "XML file path", args);
+            var linksFilePath = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "Links file path", args);
+            var defaultDocumentName = Path.GetFileNameWithoutExtension(xmlFilePath);
+            var documentName = ConsoleHelpers.GetOrReadArgument(argumentIndex, $"Document name (default: {defaultDocumentName})", args);
+            if (string.IsNullOrWhiteSpace(documentName))
+            {
+                documentName = defaultDocumentName;
+            }
+            if (!File.Exists(xmlFilePath))
+            {
+                Console.WriteLine($"${xmlFilePath} file does not exist.");
+            }
+            // var xml = File.ReadAllText(xmlFilePath);
+            // var encodedXml = Encoding.UTF8.GetBytes(xml);
+            // ReadOnlySpan<byte> readOnlySpanEncodedXml = new(encodedXml);
+            var xmlReader = XmlReader.Create(xmlFilePath);
+            var linksConstants = new LinksConstants<TLinkAddress>(enableExternalReferencesSupport: true);
+            var fileMappedResizableDirectMemory = new FileMappedResizableDirectMemory(linksFilePath);
+            var unitedMemoryLinks = UnitedMemoryLinks<TLinkAddress>.DefaultLinksSizeStep;
+            const IndexTreeType indexTreeType = IndexTreeType.Default;
+            using UnitedMemoryLinks<TLinkAddress> memoryAdapter = new(fileMappedResizableDirectMemory, unitedMemoryLinks, linksConstants, indexTreeType);
+            var links = memoryAdapter.DecorateWithAutomaticUniquenessAndUsagesResolution();
+            var balancedVariantConverter = new BalancedVariantConverter<TLinkAddress>(links);
+            var storage = new DefaultXmlStorage<TLinkAddress>(links, balancedVariantConverter);
+            var importer = new Platform.Data.Doublets.Xml.XmlImporter<TLinkAddress>(storage);
+            using ConsoleCancellation cancellation = new();
+            var cancellationToken = cancellation.Token;
+            Console.WriteLine("Press CTRL+C to stop.");
+            importer.Import(xmlReader, documentName, cancellationToken); 
+            Console.WriteLine("Import completed successfully.");
+        }
+    }
+}
